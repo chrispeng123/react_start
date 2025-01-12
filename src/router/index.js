cae873f3212455cd888b91fcaa95ab079a24845d
chrispeng123 <chrispeng19990306@gmail.com>
2025-01-12 11:47:40 +0800

    dev

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,46 @@
-import {createBrowserRouter} from "react-router-dom";
+import {createBrowserRouter, Navigate} from "react-router-dom";
 import Main from "../pages/main";
 import {Component} from "react";
 import Home from "../pages/home";
+import Mall from "../pages/mall";
+import User from "../pages/user";
+import PageOne from "../pages/other/PageOne";
+import PageTwo from "../pages/other/PageTwo";
 
 const routes = [
     {
         path: "/",
         Component: Main,
         children : [
+            // 重定向
+            {
+                path: "/",
+                element: <Navigate to="/home" replace/>,
+            },
             {
                 path: "home",
                 Component: Home
+            },
+            {
+                path: "mall",
+                Component: Mall
+            },
+            {
+                path: "user",
+                Component: User
+            },
+            {
+                path: "other",
+                children: [
+                    {
+                        path: "pageOne",
+                        Component: PageOne
+                    },
+                    {
+                        path: "pageTwo",
+                        Component: PageTwo
+                    }
+                ]
             }
         ]
     }
